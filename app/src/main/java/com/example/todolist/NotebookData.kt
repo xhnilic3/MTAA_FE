accea5d1d606ed02f075fc1f23ff44fedfbb66db
xhnilic3 <j.hnilica147@gmail.com>
2021-04-19 14:00:57 +0200

    dataclass variables refactor

--- app/src/main/java/com/example/todolist/NotebookData.kt
+++ app/src/main/java/com/example/todolist/NotebookData.kt
@@ -7,13 +7,13 @@ import kotlinx.serialization.Serializable
 @Keep
 @Serializable
 data class NotebookData (
-        var notebook_id:Int,
-        var creator_id:Int,
-        var creator_date:String,
-        var notebook_type:Int,
-        var notebook_name:String,
-        var label:String?,
-        var notebook_color:String,
-        var update_date:String,
-        var collaborator_id:Int?
+        val notebook_id:Int,
+        val creator_id:Int,
+        val creator_date:String,
+        val notebook_type:Int,
+        val notebook_name:String,
+        val label:String?,
+        val notebook_color:String,
+        val update_date:String,
+        val collaborator_id:Int?
 )
